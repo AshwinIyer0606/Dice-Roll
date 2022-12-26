61dc734dd4116862d9eba94ad9a811d436b006ec
AshwinIyer0606 <114479440+AshwinIyer0606@users.noreply.github.com>
2022-12-26 23:29:48 +0530

    Create Dice Roll.py

---
+++ Dice Roll.py
@@ -0,0 +1,12 @@
+import random
+
+num1 = int(input("Enter lower limit for the dice: "))
+num2 = int(input("Enter upper limit for the dice: "))
+while True:
+        a1 = int(input("Type 1 to get a number: "))
+	if a1 == 1:
+		print(random.randint(num1, num2))
+	elif ValueError:
+		print("Enter number 1")
+	else:
+		print("Thank You")
